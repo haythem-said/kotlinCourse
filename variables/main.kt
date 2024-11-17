65a9a108efcd1a8561c43cdde863404333126236
haythem-said <haythem.said97@gmail.com>
2024-11-17 12:22:53 +0100

    first commit

---
+++ variables/main.kt
@@ -0,0 +1,14 @@
+//  1- Variables et Types de Données : 
+        // on un deux type de variable ( var et val )
+
+//  2- Types de données: Voici quelques types de données courants en Kotlin:
+         // Int Double Boolean String
+ 
+fun main(){
+ var mutableVar:Int=10 // mutable variable on peux le modifer 
+ val immutableVar:Int=10 // imutable variable on ne peux pas changer cette valeur 
+ val PiVal:Double=3.14 // valeur de type double
+
+  mutableVar=20 // on change la varibale mutableVar de 10 par 20
+}
+
